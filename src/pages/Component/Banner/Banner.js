70b28eff87b18a14dcd93fea9e18e48a4c8b6789
sanket kanani <kananisanket2000@gmail.com>
2022-02-19 16:54:33 +0530

    create a banner component UI

---
+++ src/pages/Component/Banner/Banner.js
@@ -0,0 +1,27 @@
+import * as React from "react"
+import { Button, Col, Container, Row } from 'react-bootstrap';
+import BannerImg from '../../../images/Banner/hero.png';
+import './Banner.scss'
+
+// markup
+const Banner = () => {
+    return (
+        <div className="banner_section">
+            <Container>
+                <Row>
+                    <Col className="left_content" lg="7">
+                        <h4>JAMSTACK WEBSITES</h4>
+                        <h1>People and search engines <p>love fast websites.</p></h1>
+                        <p>Get a competitive edge (unfair advantage) over your competitors with fast, SEO friendly performant websites.</p>
+                        <Button>View More</Button>
+                    </Col>
+                    <Col className="img_content" lg="5">
+                        <img src={BannerImg} />
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    )
+}
+
+export default Banner
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import * as React from "react"
+import Banner from "./Component/Banner/Banner";
+import './Index.scss'
 
 // styles
 const pageStyles = {
   color: "#232129",
   padding: 10,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
+  // fontFamily: "-apple-system, Roboto, sans-serif, serif",
+  background: "#100818",
+  color: "#ffffffe6",
+  fontFamily: "'Poppins', sans-serif"
 }
 
 // markup
@@ -12,7 +17,7 @@ const IndexPage = () => {
   return (
     <main style={pageStyles}>
       <title>Home Page</title>
-      dtghjfhkhjlh
+      <Banner />
     </main>
   )
 }
