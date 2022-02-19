afa6eb7099d6a1029a54e384995f3b5574aff54f
sanket kanani <kananisanket2000@gmail.com>
2022-02-19 15:32:46 +0530

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,20 @@
+import * as React from "react"
+
+// styles
+const pageStyles = {
+  color: "#232129",
+  padding: 10,
+  fontFamily: "-apple-system, Roboto, sans-serif, serif",
+}
+
+// markup
+const IndexPage = () => {
+  return (
+    <main style={pageStyles}>
+      <title>Home Page</title>
+      dtghjfhkhjlh
+    </main>
+  )
+}
+
+export default IndexPage
